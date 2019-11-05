efd5bf92ff2024985800a65e1cbba1086838fb05
asdf <a@b>
2019-11-04 23:34:30 -0800

    refined threading code

--- hilvl.js
+++ hilvl.js
@@ -1,32 +1,28 @@
+
+var I = 2;
+
 function hyperPerfect() {
-    var k = 2;
-    for (var i = 2; i <= 500000; i++) {
-        var arr = getDivisors(i);
-        var sumArr = sumDivisors(arr);
+    while(I<=500000){
+        var sumArr = getDivisors(I);
 
         var rhs = (1 + 2 * (sumArr - 1));
-        if (i == rhs ) {
-            console.log(i.toString());
+        if (I == rhs ) {
+
+            console.log(I.toString());
         }
+        //lock and unlock this part
+        I++;
     }
-}
-
-
-function sumDivisors(arr) {
-    var sum = 0;
-    for (var i = 0; i < arr.length; i++) {
-        sum += arr[i];
-    }
-
-    return sum;
 }
 
 function getDivisors(  n) {
     var rett = [];
+    var sum =0 ;
+
     for (var i = 1; i < n; i++) {
         if (n%i===0) {
-            rett.push(i);
+            sum +=i ;
         }
     }
-    return rett; 
+    return sum 
 }
